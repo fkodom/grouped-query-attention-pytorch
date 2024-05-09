a80f53854e15cf461d8b9ab4b5fc6fad64b6d7db
Frank Odom <frank.odom.iii@gmail.com>
2024-05-09 13:55:38 -0500

    Fix discrepancy between grouped-SDPA and SDPA

--- tests/test_attention.py
+++ tests/test_attention.py
@@ -1,17 +1,19 @@
 import pytest
 import torch
+import torch.nn.functional as F
 
 from grouped_query_attention_pytorch.attention import (
     MultiheadGQA,
     scaled_dot_product_gqa,
 )
 
+torch.backends.cudnn.deterministic = True
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-DTYPE = torch.float16 if torch.cuda.is_available() else torch.float32
+DTYPE = torch.float64
 SEQ_LEN = 16
 
 
-@pytest.mark.parametrize("embed_dim", [32])
+@pytest.mark.parametrize("embed_dim", [64])
 @pytest.mark.parametrize("num_heads", [4, 8])
 @pytest.mark.parametrize("kv_heads", [4, 8])
 @pytest.mark.parametrize("is_causal", [True, False])
@@ -34,12 +36,20 @@ def test_grouped_scaled_dot_product_attention(
     )
     assert out.size(0) == 1
     assert out.size(1) == SEQ_LEN
-    assert out.size(2) == kv_heads
+    assert out.size(2) == num_heads
     assert out.size(3) == embed_dim
     assert attn_weights.size(0) == 1
     assert attn_weights.size(1) == SEQ_LEN
     assert attn_weights.size(2) == SEQ_LEN
-    assert attn_weights.size(3) == kv_heads
+    assert attn_weights.size(3) == num_heads
+
+    # Test that grouped SDPA is equivalent to SDPA if we duplicate the KV heads.
+    kv = kv.repeat_interleave(num_heads // kv_heads, dim=2)
+    kv = kv.permute(0, 2, 1, 3)
+    x = x.permute(0, 2, 1, 3)
+    out_vanilla = F.scaled_dot_product_attention(x, kv, kv, is_causal=is_causal)
+    out_vanilla = out_vanilla.permute(0, 2, 1, 3)
+    torch.testing.assert_close(out, out_vanilla)
 
 
 @torch.no_grad()
